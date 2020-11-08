d1be171901a2d525d5e174b31515b128884598b7
Christian Stenzel <christianstenzel@linux.com>
2020-11-08 08:58:40 +0100

    add test for input validation

--- tests/test_console.py
+++ tests/test_console.py
@@ -11,3 +11,55 @@ def runner():
 def test_main_succeeds(runner):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
+
+def test_main_succeeds_with_debug(runner):
+    result = runner.invoke(console.main, "--debug")
+    assert result.exit_code == 0
+
+def test_main_succeeds_with_input_ovp_ocp_vout_iout(runner):
+    result = runner.invoke(console.main, "--ovp 10 --ocp 10 --vout 10 --iout 10")
+    assert result.exit_code == 0
+
+def test_main_succeeds_with_input_ovp_ocp_smaller_then_vout_iout(runner):
+    result = runner.invoke(console.main, "--ovp 8 --ocp 8 --vout 10 --iout 10")
+    assert result.exit_code == 0
+
+def test_main_succeeds_with_input_vout_to_max(runner):
+    result = runner.invoke(console.main, "--vout 30")
+    assert result.exit_code == 0
+
+def test_main_succeeds_with_input_iout_to_max(runner):
+    result = runner.invoke(console.main, "--iout 10")
+    assert result.exit_code == 0
+
+def test_main_succeeds_with_input_vout_range_max(runner):
+    result = runner.invoke(console.main, "--vout 30")
+    assert result.exit_code == 0
+
+def test_main_succeeds_with_input_vout_range_min(runner):
+    result = runner.invoke(console.main, "--vout 0")
+    assert result.exit_code == 0
+
+def test_main_succeeds_with_input_vout_out_of_range_max(runner):
+    result = runner.invoke(console.main, "--vout 50")
+    assert result.exit_code != 0
+
+def test_main_succeeds_with_input_vout_out_of_range_min(runner):
+    result = runner.invoke(console.main, "--vout -50")
+    assert result.exit_code != 0
+
+def test_main_succeeds_with_input_iout_out_range_max(runner):
+    result = runner.invoke(console.main, "--iout 10")
+    assert result.exit_code == 0
+
+def test_main_succeeds_with_input_iout_out_range_min(runner):
+    result = runner.invoke(console.main, "--iout 0")
+    assert result.exit_code == 0
+
+def test_main_succeeds_with_input_iout_out_of_range_max(runner):
+    result = runner.invoke(console.main, "--iout 50")
+    assert result.exit_code != 0
+
+def test_main_succeeds_with_input_iout_out_of_range_min(runner):
+    result = runner.invoke(console.main, "--iout -50")
+    assert result.exit_code != 0
