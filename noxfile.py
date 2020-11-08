c190bb8c8964cff7be2e14c955c4ee6ffa7df3dd
Christian Stenzel <christianstenzel@linux.com>
2020-11-08 15:21:19 +0100

    Improve testiness, e.g.  python -m nox -r -- tests/test_console.py

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,8 @@
 # noxfile.py
 import nox
 
-
 @nox.session(python=['3.9.0', '3.8.2', '3.6.9'])
 def tests(session):
+    args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
-    session.run("pytest", "--cov")
+    session.run("pytest", *args)
